c1d99878ab599bcff3509363fcf7ce9f2499a9be
juis6 <olegzozulia129@gmail.com>
2025-11-12 13:08:28 +0200

    Starting server setup

--- server/index.js
+++ server/index.js
@@ -4,6 +4,13 @@ const PORT = process.env.PORT
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.json({
+        msg: "Server is running!",
+        status: true
+    })
+})
+
 app.listen(() => {
     console.log(`Server is listening on port ${PORT}`)
     console.log(`Following link http://localhost:${PORT}`)
