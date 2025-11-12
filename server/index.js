e3a8198587b8f5b22d9f65fc9cd1fb7c9068b824
juis6 <olegzozulia129@gmail.com>
2025-11-12 13:34:34 +0200

    Dot enviroment setup

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
+import dotenv from 'dotenv'
 import express from 'express'
+import morgan from 'morgan'
 
-const PORT = process.env.PORT
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+dotenv.config({ path: join(__dirname, '..', '.env') })
+
+const PORT = process.env.PORT || 8000
 
 const app = express()
 
+app.use(express.json)
+app.use(morgan)
+
 app.get('/', (req, res) => {
     res.json({
-        msg: "Server is running!",
-        status: true
+        status: 'OK'
     })
 })
 
